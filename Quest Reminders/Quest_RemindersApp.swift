3c46e8f1d08e90d8cdc629a92269f1c74dfc3d8f
Dawson Delap <dawsondelap97@gmail.com>
2024-12-13 12:15:37 -0700

    app

---
+++ Quest Reminders/Quest_RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quest_RemindersApp.swift
+//  Quest Reminders
+//
+//  Created by Dawson Delap on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Quest_RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Quest RemindersTests/Quest_RemindersTests.swift
@@ -0,0 +1,17 @@
+//
+//  Quest_RemindersTests.swift
+//  Quest RemindersTests
+//
+//  Created by Dawson Delap on 12/12/24.
+//
+
+import Testing
+@testable import Quest_Reminders
+
+struct Quest_RemindersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
